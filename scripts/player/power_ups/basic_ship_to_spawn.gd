cbaddab5a6d50dd741401276d73245dec4ee5e2c
hamletrpg <hamletgil08@gmail.com>
2025-02-20 20:57:26 -0500

    I won't be using ray tracing anymore lol, just a few collitions and check

--- scripts/player/power_ups/basic_ship_to_spawn.gd
+++ scripts/player/power_ups/basic_ship_to_spawn.gd
@@ -4,7 +4,6 @@ var dynamic_y_value_for_ray: float = 20.0
 @onready var ship_sprite_one_up: Sprite2D = $"ship_up"
 @onready var ship_sprite_two_down: Sprite2D = $"ship_down"
 
-
 @export var bullet_scene: PackedScene
 
 signal shoot(dir)
@@ -12,16 +11,6 @@ signal shoot(dir)
 func _ready():
 	self.connect("shoot",  Callable(self, "_on_shoot"))
 
-func _physics_process(delta):
-	var space_state = get_world_2d().direct_space_state
-	var query_sprite_up = PhysicsRayQueryParameters2D.create(self.ship_sprite_one_up.global_position, Vector2(2000, self.ship_sprite_one_up.global_position.y))
-	var query_sprite_down = PhysicsRayQueryParameters2D.create(self.ship_sprite_two_down.global_position, Vector2(2000, self.ship_sprite_two_down.global_position.y))
-	var result_1 = space_state.intersect_ray(query_sprite_up)
-	var result_2 = space_state.intersect_ray(query_sprite_down)
-	if result_1:
-		shoot.emit(result_1.normal)
-	if result_2:
-		shoot.emit(result_2.normal)
 	
 func _on_shoot(dir: Vector2):
 	var bullet_instance = bullet_scene.instantiate()
