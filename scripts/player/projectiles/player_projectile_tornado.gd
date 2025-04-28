cf32790c053c7a336abf36068943e06ed658edb9
hamletrpg <hamletgil08@gmail.com>
2025-04-28 00:02:01 -0400

    did progress on tornado resolution, need to refactor how damage is calculated though

--- scripts/player/projectiles/player_projectile_tornado.gd
+++ scripts/player/projectiles/player_projectile_tornado.gd
@@ -10,6 +10,9 @@ var upper_bullet: bool = true
 var current_state = State.SHOT
 var current_direction
 
+@onready var tornado_damage_timer: Timer = Timer.new()
+var timer_hit_count: int = 0
+
 enum Direction {
 	UP,
 	DOWN
@@ -31,6 +34,10 @@ var wave_vector: Vector2
 var time_flying = 0
 
 func _ready():
+	add_child(tornado_damage_timer)
+	tornado_damage_timer.connect("timeout", Callable(self, "_on_tornado_damage_timer_timeout"))
+	tornado_damage_timer.wait_time = 0.1
+
 	connect("body_entered", Callable(shooter, "_from_green_bullet_on_body_entered").bind(self))
 	bullet_position = position
 	if upper_bullet:
@@ -45,19 +52,22 @@ func _ready():
 # Although it's weird lol
 func bullet_impacted():
 	set_state(1)
+	tornado_damage_timer.start()
+	tornado_damage_timer.wait_time = 1
 
 func _process(delta): 
 	if get_state() == State.SHOT:
 		shot_state_handler(delta)
-	if get_state() == State.COLLISION:
-		collision_state_handler(delta)
+	# do I even need to handle this logic under the process/update method?
+	#if get_state() == State.COLLISION:
+		#collision_state_handler(delta)
 
 func get_state():
 	return current_state
 
 func set_state(state):
 	current_state = state
-	
+
 func get_direction():
 	return current_direction
 
@@ -79,15 +89,22 @@ func shot_state_handler(delta):
 
 '''
 What TODO when collided?
-Brainstorm:z
+Brainstorm:
 	- on contact the behavior of the bullet must change
 	- Slow Down, move one pixel - deal damage, move another pixel - deal damage, move another pixel, explode
 	- One way on checking if bullet goes up and down is:
 		keep track of center.y position of the player
 		DOWN is positive y - Up is Negative y
 '''
-func collision_state_handler(delta):
+
+func collision_state_handler():
+	if timer_hit_count >= 3:
+		queue_free()
 	if get_direction() == 1:
-		position += Vector2(1, 1) * 5 * delta
+		position += Vector2(1, 1) * 2
 	elif get_direction() == 0:
-		position += Vector2(1, -1) * 5 * delta
+		position += Vector2(1, -1) * 5 
+
+func _on_tornado_damage_timer_timeout():
+	collision_state_handler()
+	timer_hit_count += 1
